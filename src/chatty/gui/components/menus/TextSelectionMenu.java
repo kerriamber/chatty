c552fdfb4a5e5124ec561c8adf26778f3b608f39
Kerri Salciccioli <kerri@salciccio.li>
2020-02-26 16:33:19 -0500

    Add translate option on text selection menu

--- src/chatty/gui/components/menus/TextSelectionMenu.java
+++ src/chatty/gui/components/menus/TextSelectionMenu.java
@@ -1,9 +1,13 @@
 
 package chatty.gui.components.menus;
 
+import chatty.gui.UrlOpener;
 import chatty.lang.Language;
 import chatty.util.MiscUtil;
+
 import java.awt.event.ActionEvent;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * For adding a copy or copy/cut/paste context menu.
@@ -25,6 +29,7 @@ public class TextSelectionMenu extends ContextMenu {
     public TextSelectionMenu(String selectedText) {
         this.selectedText = selectedText;
         addItem("copy", Language.getString("textCm.copy"));
+        addItem("translate", Language.getString("textCm.translate"));
     }
     
     /**
@@ -34,6 +39,7 @@ public class TextSelectionMenu extends ContextMenu {
         addItem("copy", Language.getString("textCm.copy"));
         addItem("cut", Language.getString("textCm.cut"));
         addItem("paste", Language.getString("textCm.paste"));
+        addItem("translate", Language.getString("textCm.translate"));
         this.selectedText = null;
     }
     
@@ -41,6 +47,13 @@ public class TextSelectionMenu extends ContextMenu {
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("copy") && selectedText != null) {
             MiscUtil.copyToClipboard(selectedText);
+        } else if (e.getActionCommand().equals("translate") && selectedText != null) {
+            try {
+                UrlOpener.openUrl("https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=" +
+                        URLEncoder.encode(selectedText, "UTF-8"));
+            } catch (UnsupportedEncodingException ignored) {
+
+            }
         }
     }
     
